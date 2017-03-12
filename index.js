80067ff1a346d75548610e99d01db975ad14f4c7
piotr.knapczyk <piotr.knapczyk@me.com>
2017-03-12 15:44:38 +0100

    databse fallback

--- index.js
+++ index.js
@@ -16,13 +16,19 @@ app.get('/', function(request, response) {
 var pg = require('pg');
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
+  	if (err) {
+  		console.error(err);
+  		response.send(err);
+  	}else {
+	  	client.query('SELECT * FROM test_table', function(err, result) {
+	      done();
+	      if (err)
+	       { console.error(err); response.send("Error " + err); }
+	      else
+	       { response.render('pages/db', {results: result.rows} ); }
+	    });
+
+  	}
   });
 });
 
