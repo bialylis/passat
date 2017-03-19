6b998a23832bd02f3dc8161c076c7a234a9e2922
Michał Pluta <m.pluta95@gmail.com>
2017-03-19 16:29:08 +0100

    Added origin for heroku frontend app

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/backend/views');
 app.set('view engine', 'ejs');
 
 var corsOptions = {
-  origin: ['http://localhost:5000', 'https://localhost:5000'],
+  origin: ['http://localhost:5000', 'https://localhost:5000', 'https://passat.herokuapp.com'],
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
