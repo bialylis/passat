fd9ec6e5093adfbcd5cb2a395fecf9a1d26a8e6d
piotr.knapczyk <piotr.knapczyk@me.com>
2017-03-19 13:54:33 +0100

    Example endpoint localhost:5001/test

--- start.sh
+++ start.sh
@@ -1,9 +1,2 @@
  node backend/index.js &
- p1=$!	
  node frontend/server.js &
-p2=$!
-
-trap 'kill "$p1"; kill "$p2"' SIGINT
-while kill -s 0 "$p1" || kill -s 0 "$p2"; do
-    wait "$p1"; wait "$p2"
-done &>/dev/null
